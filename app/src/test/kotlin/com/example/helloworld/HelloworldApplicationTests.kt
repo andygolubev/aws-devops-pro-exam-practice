112c5a6811b89cb3b8560833d566de15adab8f28
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-03-19 20:56:15 +0200

    correct test

--- app/src/test/kotlin/com/example/helloworld/HelloworldApplicationTests.kt
+++ app/src/test/kotlin/com/example/helloworld/HelloworldApplicationTests.kt
@@ -1,13 +1,33 @@
 package com.example.helloworld
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HelloworldApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
+    @Test
+    fun contextLoads() {
+    }
+
+    @Test
+    fun `test hello world endpoint`() {
+        mockMvc.perform(get("/hello"))
+            .andExpect(status().isOk)
+            .andExpect(content().json("""{"message":"Hello, World!"}"""))
+    }
 }
